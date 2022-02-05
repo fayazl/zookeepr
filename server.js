3bee15d2e5673a07b137a02b2246091967fbfa00
Fayaz <flalani100@gmail.com>
2022-02-04 23:14:57 -0500

    Added 404

--- server.js
+++ server.js
@@ -17,6 +17,11 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+  function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+  }
+
 
   app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -25,6 +30,16 @@ function filterByQuery(query, animalsArray) {
     }
     res.json(results);
   });
+
+  app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+      res.json(result);
+    } else {
+      res.sendStatus(404);
+    }
+  });
+
 
   function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
