1826d5b88ab0b7b34def3b60940dfe918b19bd8c
Fayaz <flalani100@gmail.com>
2022-02-05 00:02:47 -0500

    Added POST request and data validation

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const express = require('express');
 const { animals } = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const fs = require('fs');
+const path = require('path');
+
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
 
 function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
@@ -22,6 +30,15 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
+  function createNewAnimal(body, animalsArray) {
+    const animal = body;
+    animalsArray.push(animal);
+    fs.writeFileSync(
+      path.join(__dirname, './data/animals.json'),
+      JSON.stringify({ animals: animalsArray }, null, 2)
+    );
+    return animal;
+  }
 
   app.get('/api/animals', (req, res) => {
     let results = animals;
@@ -40,6 +57,19 @@ function filterByQuery(query, animalsArray) {
     }
   });
 
+  app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateAnimal(req.body)) {
+      res.status(400).send('The animal is not properly formatted.');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+  });
+  
 
   function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -80,7 +110,23 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
-  
+  function validateAnimal(animal) {
+    if (!animal.name || typeof animal.name !== 'string') {
+      return false;
+    }
+    if (!animal.species || typeof animal.species !== 'string') {
+      return false;
+    }
+    if (!animal.diet || typeof animal.diet !== 'string') {
+      return false;
+    }
+    if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+      return false;
+    }
+    return true;
+  }
+
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
